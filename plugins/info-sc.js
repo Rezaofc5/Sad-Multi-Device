20e0afb21ded2212d39bda56f177037a3e22c7d8
Rezaofc2 <106691452+Rezaofc2@users.noreply.github.com>
2022-06-17 12:52:55 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/FahriAdison/Sad-Multi-Device*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
-conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+let str = `*NYARI SC BANH?*\nBUAT SENDIRI LAH PUNYA TANGAN KAN? \nMAU ENAK DOANG ANJ`
+conn.sendHydrated(m.chat, str, wm, pp, 'CARI SENDIRI ASU', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 }
