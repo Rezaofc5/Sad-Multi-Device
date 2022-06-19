aab7765d8d69e621c1895c5bacdeee02a09fc7aa
Rezaofcbotmd <106826313+Rezaofcbotmd@users.noreply.github.com>
2022-06-20 02:31:55 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*NYARI SC DEK?*\nLU PUNYA TANGAN KAN? \nKALAU PUNYA TANGAN BUAT SENFIRI LAH ANJ`
-conn.sendHydrated(m.chat, str, wm, pp, 'https://doujindesu.xxx.com', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+let str = `*NYARI SC DEK?*\nLU PUNYA TANGAN KAN? \nKALAU PUNYA TANGAN BUAT SENDIRI LAH ANJ`
+conn.sendHydrated(m.chat, str, wm, pp, 'https://doujindesu.xxx.com', 'SITUS WEB', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 }
